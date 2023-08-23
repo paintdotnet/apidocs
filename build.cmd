b18e1b531db9e006186d460b639e23dbc15db44d
Rick Brewster <rickbrew@users.noreply.github.com>
2023-08-23 13:03:47 -0700

    Test commit for GH pages

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
 @rem NOTE: Updating this batch file will NOT affect how the ApiDocs are built by MakePackages!
 @rem       See ./MakePackage/ApiDocs/ApiDocsPackageBuilder.cs
+rmdir /s /q docs
 docfx docfx.json --disableGitFeatures %*
